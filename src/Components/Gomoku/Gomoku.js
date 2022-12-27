39ed3d2ba22f03d506ac8280e7391f773703c8f5
1804011 <sharifsharifulislam061@gmail.com>
2022-12-27 20:35:06 +0600

    gomoku ai implemented

--- src/Components/Gomoku/Gomoku.js
+++ src/Components/Gomoku/Gomoku.js
@@ -1,8 +1,18 @@
+
 class Gomoku {
     constructor(position = "") {
         this.boardState = [];
         this.count = 0;
         this.special = [];
+        this.fiveInRow = 0;
+        this.nonFourInRow = 0;
+        this.fourInRow = 0;
+        this.threeInRow = 0;
+        this.brokenThree = 0;
+        this.twoInRow = 0;
+        this.singleMark = 0;
+        this.totalCount = 0;
+        this.allMoves = [];
         for (let i = 0; i < 15; i++) {
             let arr = [];
             for (let j = 0; j < 15; j++) {
@@ -11,10 +21,13 @@ class Gomoku {
                     char = 1;
                 else char = parseInt(position[15 * i + j]);
                 if (char === 1) this.count++;
+                if (char === 3) this.singleMark++;
+                if (char === 2) this.singleMark--;
                 arr.push(char);
 
             }
             this.boardState.push(arr);
+
 
         }
 
@@ -99,11 +112,221 @@ class Gomoku {
         const val = (this.turn() === 0 ? 3 : 2);
         this.boardState[i][j] = val;
         this.count--;
+        if (val === 3) this.singleMark++;
+        if (val === 2) this.singleMark--;
         return this.toBoardString();
 
 
     }
+    func5(arr) {
+        if (!Array.isArray(arr)) return;
+        if (arr.length < 5) return;
+        let mul1 = 1, mul2 = 1;
+        for (let i = 0; i < 5; i++) {
+            mul1 *= arr[i];
+        }
+        for (let i = 1; i < 4; i++) mul2 *= arr[i];
+        if (mul1 === 243) this.fiveInRow++;
+        else if (mul1 === 32) this.fiveInRow--;
+        else if (mul1 === 81) this.fourInRow++;
+        else if (mul1 === 16) this.fourInRow--;
+        else if (mul1 === mul2 && mul1 === 27) this.threeInRow++;
+        else if (mul1 === mul2 && mul1 === 8) this.threeInRow--;
+        else if (mul1 === mul2 && mul1 === 9 && arr[2] === 3) this.twoInRow++;
+        else if (mul1 === mul2 && mul1 === 4 && arr[2] === 2) this.twoInRow--;
+    }
+    func6(arr) {
+        if (!Array.isArray(arr) || arr.length < 6) return;
+        let mul1 = 1, mul2 = 1;
+        for (let i = 0; i < 6; i++) mul1 *= arr[i];
+        for (let i = 1; i < 5; i++) mul2 *= arr[i];
+        if (mul1 === mul2 && mul1 === 81) this.nonFourInRow++;
+        else if (mul1 === mul2 && mul1 === 16) this.nonFourInRow--;
+        else if (mul1 === 162) this.fourInRow++;
+        else if (mul1 === 48) this.fourInRow--;
+        else if (mul1 === mul2 && mul1 === 27) this.brokenThree++;
+        else if (mul1 === mul2 && mul1 === 8) this.brokenThree--;
+
+    }
+    evaluate() {
+        let weight = 0;
+        if (this.isWin() === 1) return 100000000;
+        else if (this.isWin() === 2) return -100000000;
+        for (let i = 0; i < 15; i++) {
+            for (let j = 0; j < 15; j++) {
+                let arr1 = [];
+                let arr2 = [], arr3 = [], arr4 = [];
+                for (let k = 0; k < 5; k++) {
+                    if (this.valid(i, j + k))
+                        arr1.push(this.boardState[i][j + k]);
+                    if (this.valid(i + k, j))
+                        arr2.push(this.boardState[i + k][j]);
+                    if (this.valid(i + k, j + k))
+                        arr3.push(this.boardState[i + k][j + k]);
+                    if (this.valid(i + k, j - k))
+                        arr4.push(this.boardState[i + k][j - k]);
+                }
+                this.func5(arr1); this.func5(arr2); this.func5(arr3); this.func5(arr4)
+                let a1 = [], a2 = [], a3 = [], a4 = [];
+                for (let k = 0; k < 6; k++) {
+                    if (this.valid(i, j + k))
+                        a1.push(this.boardState[i][j + k]);
+                    if (this.valid(i + k, j))
+                        a2.push(this.boardState[i + k][j]);
+                    if (this.valid(i + k, j + k))
+                        a3.push(this.boardState[i + k][j + k]);
+                    if (this.valid(i + k, j - k))
+                        a4.push(this.boardState[i + k][j - k]);
+                }
+                this.func6(a1); this.func6(a2); this.func6(a3); this.func6(a4)
+            }
+        }
+        return 100000000 * this.fiveInRow +
+            1000000 * this.nonFourInRow +
+            10000 * this.fourInRow +
+            100 * this.threeInRow +
+            10 * this.brokenThree +
+            5 * this.twoInRow +
+            this.singleMark
+
+
+    }
+    moves() {
+        this.allMoves = [];
+        const dr = [0, 0, 0, 0, -2, -1, 1, 2, -1, -2, -1, -2, 1, 2, 1, 2]
+        const dc = [-2, -1, 1, 2, 0, 0, 0, 0, -1, -2, 1, 2, -1, -2, 1, 2]
+        for (let i = 0; i < 15; i++) {
+            for (let j = 0; j < 15; j++) {
+                if (this.boardState[i][j] > 1) continue;
+                let mul = 1;
+                dr.forEach((x, idx) => {
+                    const u = x;
+                    const v = dc[idx];
+                    if (this.valid(i + u, j + v))
+                        mul *= this.boardState[i + u][j + v];
+
+                })
+                if (mul > 1)
+                    this.allMoves.push({ i, j })
+            }
+        }
+        this.allMoves.sort((a, b) => {
+            const gomoku = new Gomoku(this.toBoardString());
+            gomoku.move(a.i, a.j);
+            const gomoku2 = new Gomoku(this.toBoardString());
+            gomoku2.move(b.i, b.j);
+            const result1 = gomoku.evaluate();
+            const result2 = gomoku2.evaluate();
+            if (result1 > result2)
+                return -1;
+            if (result1 === result2) {
+                if (Math.abs(a.i - 7) + Math.abs(a.j - 7) <= Math.abs(b.i - 7) + Math.abs(a.i - 7))
+                    return -1;
+                return 1;
+            }
+            return 1;
+
+
+        })
+        return this.allMoves;
+
+    }
+
 
 }
+const alphaBeta = (board, depth, isMaximise, alpha = -Infinity, beta = Infinity) => {
+    const gomoku = new Gomoku(board);
+    if (gomoku.isWin() || gomoku.isDraw() || depth === 0) return gomoku.evaluate();
+    if (isMaximise) {
+        let mx = -Infinity;
+        let allMoves = gomoku.moves();
 
+        for (let k = 0; k < allMoves.length; k++) {
+            const { i, j } = allMoves[k];
+            const newGomoku = new Gomoku(board);
+            newGomoku.move(i, j);
+            const way = alphaBeta(newGomoku.toBoardString(), depth - 1, 1 - isMaximise, alpha, beta)
+            mx = Math.max(mx, way);
+            alpha = Math.max(alpha, mx);
+            if (beta <= alpha) { break; }
+        }
+        return mx;
+    }
+    else {
+        let mn = Infinity;
+        const allMoves = gomoku.moves().reverse();
+        const size = allMoves.length;
+        for (let k = 0; k < size; k++) {
+            const { i, j } = allMoves[k];
+            const newGomoku = new Gomoku(board);
+            let way = alphaBeta(newGomoku.move(i, j), depth - 1, 1 - isMaximise, alpha, beta)
+            mn = Math.min(mn, way);
+            beta = Math.min(mn, beta);
+            if (beta <= alpha) { break; }
+        }
+        return mn;
+    }
+}
+export const bestMoves = (board, depth, isMaximise, alpha = -Infinity, beta = Infinity) => {
+    let bestMoves = [];
+    let allMoves = new Gomoku(board).moves();
+    if (isMaximise) {
+        let mx = -Infinity;
+        if (allMoves.length === 0) {
+            allMoves = [
+                { i: 6, j: 6 },
+                { i: 6, j: 7 },
+                { i: 6, j: 8 },
+                { i: 7, j: 6 },
+                { i: 7, j: 7 },
+                { i: 7, j: 8 },
+                { i: 8, j: 6 },
+                { i: 8, j: 7 },
+                { i: 8, j: 8 },
+            ]
+        }
+        let size = allMoves.length;
+        for (let k = 0; k < size; k++) {
+            const { i, j } = allMoves[k];
+
+            let way = alphaBeta(new Gomoku(board).move(i, j), depth - 1, 1 - isMaximise);
+            if (way > mx) {
+                bestMoves = []
+                mx = way;
+                bestMoves.push({ i, j, mx });
+            }
+            else if (way === mx)
+                bestMoves.push({ i, j, mx });
+            alpha = Math.max(alpha, mx);
+            if (beta <= alpha) break;
+
+        }
+
+
+
+
+    }
+    else {
+        let mn = Infinity;
+        allMoves = allMoves.reverse();
+        const size = allMoves.length;
+        for (let k = 0; k < size; k++) {
+            const { i, j } = allMoves[k];
+            const way = alphaBeta(new Gomoku(board).move(i, j), depth - 1, 1 - isMaximise)
+            if (way < mn) {
+                bestMoves = [];
+                mn = way;
+                bestMoves.push({ i, j, mn });
+            }
+            else if (way === mn)
+                bestMoves.push({ i, j, mn });
+        }
+
+    }
+    return bestMoves[Math.floor(Math.random() * bestMoves.length)];
+
+
+
+
+}
 export default Gomoku;
--- src/Components/Gomoku/GomokuMain.js
+++ src/Components/Gomoku/GomokuMain.js
@@ -1,32 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import Gomoku from './Gomoku';
+import Gomoku, { bestMoves } from './Gomoku';
 import GomokuBoard from './GomokuBoard';
 
 const GomokuMain = () => {
     const [gomoku, setGomoku] = useState(new Gomoku());
     const [special, setSpecial] = useState(gomoku.special);
+    const [clickAble, setClickAble] = useState(false);
     useEffect(() => {
         if (gomoku.isWin()) {
             setSpecial([...gomoku.special]);
+            alert('Player ' + gomoku.isWin() + " win");
+            return;
         }
         else if (gomoku.isDraw()) {
-            alert("Draw")
+            alert("Draw");
+            return;
         }
+        setTimeout(() => {
+            if (gomoku.turn() === 0) {
+                const { i, j } = bestMoves(gomoku.toBoardString(), 2, 1);
+                setGomoku(new Gomoku(gomoku.move(i, j)))
+                setClickAble(true);
+            }
+        }, [100])
+
+
 
     }, [gomoku])
     const onCellClick = (r, c) => {
-
+        // alert(r, c);
         const result = gomoku.move(r, c);
 
         if (result) {
             setGomoku(new Gomoku(result))
+            setClickAble(false);
         }
 
     }
-    console.log(gomoku.special);
+
     return (
         <div style={{ margin: "48px" }}>
-            <GomokuBoard onClick={onCellClick} position={gomoku.toBoardString()} special={special} />
+            <GomokuBoard onClick={clickAble ? onCellClick : () => { }} position={gomoku.toBoardString()} special={special} />
         </div>
     );
 };
